563919e14bb4597be3868fe253c03c25b9af6103
AndresDardex <jaimea.salcedov@konradlorenz.edu.co>
2023-04-19 20:10:06 -0500

    correccion de errores

---
+++ src/ginijava/ApiConnectionPerCountry.java
@@ -0,0 +1,35 @@
+package ginijava;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class apiConnectionPerCountry {
+    private static final String BASE_URL = "https://restcountries.com/v3.1/name/";
+    
+    public String GetCountry(String country) throws IOException {
+        URL url = new URL(BASE_URL + country);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
+        connection.setRequestMethod("GET");
+        
+        int responseCode = connection.getResponseCode();
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : " + responseCode);
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
+    
+}
